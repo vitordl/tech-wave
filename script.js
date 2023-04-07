ae32a972f84081005e77379518e10619c87d28b9
Vitor de Lucca <vitortactic@gmail.com>
2023-04-07 18:14:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+/* let inicio = document.querySelector('#inicio')
+
+
+
+
+let servicos = document.querySelector('#servicos')
+servicos.addEventListener('click', () => {
+    servicos.style.color = 'white'
+    inicio.style.color = 'gray'
+})
+
+
+$('.nav li').click(function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+})
+
+document.querySelector('.nav li').addEventListener('click', () => {
+    document.querySelector('.nav li').removeClass('active')
+    
+})
+
+
+
+ */
+
+
+/*   const links = document.querySelectorAll('.navbar-nav a');
+
+  // Itera pelos links e adiciona um event listener
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      // Remove a classe "active" do link atualmente ativo
+      document.querySelector('.navbar-nav .active').classList.remove('active');
+      // Adiciona a classe "active" ao link correspondente à página atual
+      link.classList.add('active');
+    });
+  }); 
+  
+  algo como:
+  
+  */
+
+
+ 
+
+
+
